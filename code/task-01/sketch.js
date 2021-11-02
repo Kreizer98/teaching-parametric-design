7c5a9b5fff7f319cc96f2a704ae9b18dbd2be90e
Martin <55854023+Kreizer98@users.noreply.github.com>
2021-11-02 15:20:39 +0100

    Hausaufgabe, random Sachen spawnen

--- code/task-01/sketch.js
+++ code/task-01/sketch.js
@@ -11,6 +11,8 @@ function draw() {
   fill(random(124), random(252), random(0)); //radom Farbwahl from 0 to 124,252,0
   rect(random(400), random(400), 55, 55, 20);
   circle(random(400), random(400), 20, 20, 20);
+  line(random(350),random(350),random(100),random(200));
+  stroke(136);
   textSize(random(70)); //max size up to 70
   text(word, random(400), random(400)); // write word down inside 400x400
 }
